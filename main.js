89ce79c1dc11f9b553d143b4431ab390a8abcf52
Mon Pampín <mon.medialab@gmail.com>
2024-01-14 17:22:42 +0100

    cleaning2

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 'use strict';
 
+//? Functions: ===================================================
+let totalTime = 3;
+//Cuenta atrás --------------------------------------------------
+  function updateClock() {
+    document.getElementById('cuenta-atras').innerHTML = totalTime;
+    if (!(totalTime === 0)) {totalTime--;}
+  }
+
+  //reloadPage ----------
+  function reloadPage() {
+    location.reload(true);
+  }
+  
+  //overlayClok() --------------------------------------------
+  function overlayClok() {
+    document.getElementById('overlay').style.display = 'block';
+    setTimeout(reloadPage, 4000);
+    setInterval(updateClock, 1000);
+    inputLetra.disabled = true;
+  }
+//? fin Functions: ===============================================
+
+
 /* const palabras = ['js', 'javascript', 'hackaboss']; */
 const palabras = ['js', 'css'];
 let letrasPalabraAleatoriaEnGuiones = [];
-//let letra = '';
+let localStrg2 = '';
+let fallos = 0;
 
 let form = document.querySelector('.my-form');
 
-let localStrg2 = '';
-let fallos = 0;
-//let fallosTotal = 0;
-//document.querySelector('#inputLetra').focus();//ir al foco
+document.querySelector('#inputLetra').focus(); //ir al foco
 
+
+// ? palabraAleatoria
 const palabraAleatoria = palabras[Math.floor(Math.random() * palabras.length)];
-//console.log('2:palabraAleatoria: ' + palabraAleatoria);
+console.log('2:palabraAleatoria: ' + palabraAleatoria);
 
 
-let letrasPalabraAleatoria = palabraAleatoria.split('');
+/* let letrasPalabraAleatoria = palabraAleatoria.split('');
 //console.log('4.1:letrasPalabraAleatoria: ' + letrasPalabraAleatoria);
 
 letrasPalabraAleatoria.forEach((element) => {
@@ -25,30 +48,30 @@ letrasPalabraAleatoria.forEach((element) => {
 //console.log('letrasPalabraAleatoriaEnGuiones: ' + letrasPalabraAleatoriaEnGuiones);
 
 let palabraConGuionesSinComas = letrasPalabraAleatoriaEnGuiones.join(' ');
-//console.log('3.2 palabraConGuionesSinComas: ' + palabraConGuionesSinComas);
+//console.log('3.2 palabraConGuionesSinComas: ' + palabraConGuionesSinComas); */
 
 
-/* let palabraConGuionesSinComas = '';
-// ! 
-for (let char of letrasPalabraAleatoria) {
-   palabraConGuionesSinComas = '_ '; // replace 
-  console.log(palabraConGuionesSinComas);
-} */
+let palabraConGuionesSinComas = '';
 
+for (let char of palabraAleatoria) {
+   palabraConGuionesSinComas += '_ '; // replace 
+}
+//console.log(palabraConGuionesSinComas);
 
-
+// ? innerHTML = palabraConGuionesSinComas ---------
 let palabraHtml = document.querySelector('#palabra');
 palabraHtml.innerHTML = palabraConGuionesSinComas;
 
 
-
-// ? ================================================
+// ? Event ======================================================
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   //*cazando valor del input
   let userInput = form.valueOfAtrNameOfinput.value.toLowerCase();
   //console.log('LETRA Q INTRODUCE EL USUARIO: ' + userInput);
+  form.reset();
+  inputLetra.focus();
 
   let palabraConOcurrencias = '';
 
@@ -60,70 +83,50 @@ form.addEventListener('submit', function (e) {
     }
   }
   //console.log('palabraConOcurrencias: ' + palabraConOcurrencias);
-  inputLetra.focus();
-  form.reset();
+  
+  //? innerHTML = palabraConOcurrenciasYespacios =================================
+  let palabraConOcurrenciasArray = palabraConOcurrencias.split('');
+  let palabraConOcurrenciasYespacios = palabraConOcurrenciasArray.join(' ');
 
   const palabraConOcurrenciasEnDOM = document.querySelector('#palabra');
-
-  let palabraConOcurrenciasArray = palabraConOcurrencias.split('');
-  //console.log('palabraConOcurrenciasArray: ' + palabraConOcurrenciasArray);
-
-  let palabraConOcurrenciasYespacios = palabraConOcurrenciasArray.join(' ');
-  //console.log('palabraConOcurrenciasYespacios: ' + palabraConOcurrenciasYespacios);
-
   palabraConOcurrenciasEnDOM.innerHTML = palabraConOcurrenciasYespacios;
 
-//* PERSISTENCIA DE DATOS? ---------------------------------------------------
+
+  //? PERSISTENCIA DE DATOS? =======================================================
   localStorage.setItem('palabraConOcurrenciasEnDOM',palabraConOcurrenciasYespacios);
   localStrg2 = localStorage.getItem('palabraConOcurrenciasEnDOM');
   //console.log('localStrg2: ' + localStrg2);
 
 
-  //? fallo ==================================================================
-  if (! palabraAleatoria.includes(userInput) || localStrg2.includes(userInput)) {
+  //? fallos ==================================================================
+  if (!(palabraAleatoria.includes(userInput) || localStrg2.includes(userInput))) {//! cambio
     //console.log('fallo');
     fallos++;
-
     for (let i = 1; i <= 6; i++) {
       const fallo = document.querySelector(`#fallo${i}`);
       fallo.style.visibility = fallos === i ? 'visible' : 'hidden';
     }
     //console.log('Número de Fallos: ' + fallos);
+    /* //! PERDER ******
+    if (fallos >= 3) {
+      document.querySelector('#text').innerHTML = 'GAME OVER';    
+      overlayClok()
+  
+    }  */
   }
-
-
-  //? =============================================
+  //? innerHTML = fallos ----------------------------
   let fallosDOM = document.querySelector('#fallosID');
   fallosDOM.innerHTML = fallos;
 
-  //Cuenta atrás -----------------------------------------------
-  let totalTime = 5;
-  function updateClock() {
-    document.getElementById('cuenta-atras').innerHTML = totalTime;
-    if (!(totalTime === 0)) {totalTime--;}
-  }
 
-  //
-  function reloadPage() {
-    location.reload(true);
-  }
-
-  function overlayClok() {
-    document.getElementById('overlay').style.display = 'block';
-    setTimeout(reloadPage, 6000);
-    setInterval(updateClock, 1000);
-    inputLetra.disabled = true;
-    
-  }
-
-//! PERDER ****** MIENTRAS NO GANA,solo agregar imágenes y dibujar palabra +(overlay, reload)
+  //! PERDER *** innerHTML = 'GAME OVER' + overlay + reload 
   if (fallos >= 6) {
-    document.querySelector('#text').innerHTML = 'GAME OVER';//!  ESTO CAMBIA
+    document.querySelector('#text').innerHTML = 'GAME OVER';
     overlayClok()
-
-  //! GANAR ********************** solo overlay, reload
-  } else if (!localStrg2.includes('_')) {
-    document.querySelector('#text').innerHTML = 'WINNER'; //!  ESTO CAMBIA
+  } 
+  //! GANAR *** innerHTML = 'WINNER' + overlay + reload
+  else if (!palabraConOcurrenciasYespacios.includes('_')) {
+    document.querySelector('#text').innerHTML = 'WINNER';
     overlayClok()
   }
 
